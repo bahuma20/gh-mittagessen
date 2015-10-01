e106654ee5489a3bd877c862952a370be92c8121
Max Bachhuber <bahuma20@bahuma.io>
2015-10-01 21:45:51 +0200

    Create Demo Content

---
+++ create_demo_content.php
@@ -0,0 +1,55 @@
+<?php
+$db = new PDO('mysql:dbname=mittagesser;host=localhost', 'test', 'testpw');
+
+use Bahuma\GHMittagessen\Offer;
+use Bahuma\GHMittagessen\Participation;
+use Bahuma\GHMittagessen\Restaurant;
+use Carbon\Carbon;
+
+require_once('vendor/autoload.php');
+
+// Restaurants
+
+$restaurant1 = new Restaurant();
+$restaurant1->setName("Bella Italia");
+$restaurant1->setSpeisekartenUrl("http://test.de");
+$restaurant1->save();
+
+print "Created Restaurant 1 with id " . $restaurant1->getId();
+
+
+$restaurant2 = new Restaurant();
+$restaurant2->setName("Burgerladen");
+$restaurant2->setSpeisekartenUrl("http://test2.de");
+$restaurant2->save();
+
+print "Created Restaurant 2 with id " . $restaurant2->getId();
+
+
+// Offers
+$offer1 = new Offer();
+$offer1->setRestaurant($restaurant1->getId());
+$offer1->setUser(1);
+$offer1->setOrderUntil(new Carbon());
+$offer1->save();
+
+print "Created Offer 1 at Restaurant 1 with id " . $offer1->getId();
+
+
+$offer2 = new Offer();
+$offer2->setRestaurant($restaurant2->getId());
+$offer2->setUser(1);
+$offer2->setOrderUntil(new Carbon());
+$offer2->save();
+
+print "Created Offer 2 at Restaurant 2 with id " . $offer2->getId();
+
+
+// Participation
+$participation = new Participation();
+$participation->setUser(2);
+$participation->setOffer($offer1->getId());
+$participation->setOrder("Einmal 2 halbe Hahn.");
+$participation->save();
+
+print "Created Participation at Order 1 with id " . $participation->getId();
