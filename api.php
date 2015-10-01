0223549b9b93ac760f376a901d0a093720ffad09
Max Bachhuber <bahuma20@bahuma.io>
2015-10-01 23:09:24 +0200

    Implement first get API-Endpoints

--- api.php
+++ api.php
@@ -1,14 +1,37 @@
 <?php
 
+use Bahuma\GHMittagessen\Offer;
+use Bahuma\GHMittagessen\Participation;
+use Bahuma\GHMittagessen\Restaurant;
+
 require_once('vendor/autoload.php');
 
 $db = new PDO('mysql:dbname=mittagesser;host=localhost', 'test', 'testpw');
+
+
+function outputJSON($data) {
+    header("Content-Type: application/json");
+    echo json_encode($data);
+    exit;
+}
 
 
 $app = new \Slim\Slim();
 
 $app->get('/test', function () {
     echo "It is running!!!";
+});
+
+$app->get('/restaurants', function() {
+    outputJSON(Restaurant::getAll());
+});
+
+$app->get('/participations', function() {
+    outputJSON(Participation::getAll());
+});
+
+$app->get('/offers', function() {
+    outputJSON(Offer::getAll());
 });
 
 $app->run();
