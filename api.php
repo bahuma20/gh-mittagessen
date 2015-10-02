cb3df93b54b0bb9d59c2d9ce63522e2ceff7deaf
bahuma20 <bahuma20@bahuma-online.de>
2015-10-02 10:04:22 +0200

    Add post methods

--- api.php
+++ api.php
@@ -22,16 +22,46 @@ $app->get('/test', function () {
     echo "It is running!!!";
 });
 
-$app->get('/restaurants', function() {
+$app->get('/restaurant', function() {
     outputJSON(Restaurant::getAll());
 });
 
-$app->get('/participations', function() {
+$app->post('/restaurant', function() use ($app) {
+    $body = json_decode($app->request->getBody());
+
+    $restaurant = new Restaurant();
+    $restaurant->setName($body->name);
+    $restaurant->setSpeisekartenUrl($body->speisekarten_url);
+    $restaurant->save();
+
+});
+
+$app->get('/participation', function() {
     outputJSON(Participation::getAll());
 });
 
-$app->get('/offers', function() {
+$app->post('/participation', function() use ($app) {
+    $body = json_decode($app->request->getBody());
+
+    $participation = new Participation();
+    $participation->setOffer($body->offer);
+    $participation->setUser($body->user);
+    $participation->setOrder($body->order);
+    $participation->save();
+});
+
+$app->get('/offer', function() {
     outputJSON(Offer::getAll());
+});
+
+$app->post('/offer', function() use ($app) {
+    $body = json_decode($app->request->getBody());
+
+    $offer = new Offer();
+    $offer->setUser($body->user);
+    $offer->setRestaurant($body->restaurant);
+    $offer->setOrderUntil(new \Carbon\Carbon($body->order_until));
+    $offer->save();
 });
 
 $app->run();
